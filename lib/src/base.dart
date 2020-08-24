27ee4066ce363b0c8da1403589edcf493e71c19c
Bruno Garcia <bruno@brunogarcia.com>
2020-08-23 21:33:52 -0400

    works

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -2,10 +2,4 @@ import 'dart:io';
 
 /// The types of scores. In the order they are presented on pub.dev
 const scoreTypes = ['likes', 'pub points', 'popularity'];
-bool get isProduction => 
-
-
-
-
-
-Platform.environment['ENVIRONMENT'] == 'prod';
+bool get isProduction => Platform.environment['ENVIRONMENT'] == 'prod';
